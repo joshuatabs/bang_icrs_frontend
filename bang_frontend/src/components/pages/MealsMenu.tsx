cfd5eaea34c9a677fb2e8c94c7b7d0b35cc78d3f
RianFrancis Cabanero <rianfrancis.cabanero@cit.edu>
2022-12-09 09:46:39 +0800

    add node

---
+++ bang_frontend/src/components/pages/MealsMenu.tsx
@@ -0,0 +1,51 @@
+import '../Rian.css';
+import Sidebar from '../sidebar';
+import Card, { FoodCardDetails } from '../FoodCardDetails';
+import { Route, Routes } from 'react-router-dom';
+
+
+
+
+
+const Foodlist:FoodCardDetails[] = [
+  {title:'Sisig', image:'/static/Meals/sisig.png', description:'a Filipino dish made from parts of a pigs face and belly and chicken liver which is usually seasoned with calamansi onions and chili peppers'},
+  {title:'Jjampong', image:'/static/Meals/jjampong.png', description:'noodle soup with red, spicy seafood- or pork-based broth flavored with gochugaru. Tasty and Delicious!. Originated in South-Korea'},
+  {title:'Sizzling Steak', image:'/static/Meals/sizzlingsteak.png', description:'a cut of steak that has been thinly sliced. The most common cuts made into sizzle steak are top round and sirloin'},
+  {title:'Fried Chicken', image:'/static/Meals/friedchicken.png', description:'a dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried.'},
+  
+]
+
+const Foodlist2:FoodCardDetails[] = [
+  {title:'Baconsilog', image:'/static/Meals/baconsilog.png', description:'A combination of both eggs and strips of bacon, commonly served during breakfast it is one way to satiate one’s hunger'},
+  {title:'Burger Steak', image:'/static/Meals/burgersteak.png', description:'made of flavorful burger patties that are smothered in a rich and luxurious mushroom gravy sauce. Yummy and Delicious!'},
+  {title:'Pancit Canton', image:'/static/Meals/pancitcanton.png', description:'a type of Lo Mein or tossed noodles known as flour sticks. This dish is often served during birthdays and special occasions to symbolize long life.'},
+  {title:'Barbecue', image:'/static/Meals/bbq.png', description:'composed of thinly sliced pork pieces that are marinated in a special mixture of seasonings and spices.'},
+  
+]
+
+function MealsMenu(){
+  return (
+    
+    <div className="RianApp">
+
+      
+
+      <div className='Sidebar'><Sidebar></Sidebar></div>
+       
+      <div className='RIANCARDS'>
+          {Foodlist.map((item, i)=>
+            <Card key={i} title={item.title} image={item.image} description={item.description}/>
+          )}
+        </div>
+        <div className='RIANCARDS'>
+          {Foodlist2.map((item, i)=>
+            <Card key={i} title={item.title} image={item.image} description={item.description}/>
+          )}
+        </div>
+        
+
+    </div>
+  );
+}
+
+export default MealsMenu;
