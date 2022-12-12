3facfa8ec5237ececfd192be8fdaa1dadb4c5bcb
K3ndi647 <118802814+K3ndi647@users.noreply.github.com>
2022-12-12 10:45:59 +0800

    Create Payment.tsx

---
+++ bang_frontend/src/components/pages/Payment.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, ButtonGroup } from '@mui/material';
+import React from 'react'
+import { AiFillCreditCard } from "react-icons/ai";
+import { BsCash } from "react-icons/bs";
+import { HiCash } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+
+
+
+
+
+
+function Payment(){
+  const navigate = useNavigate();
+  return (
+    <div className="App"> 
+    <header className="App-header">
+        <h1> Select Payment </h1>
+
+        <div className='choices1'>
+
+          <Button  className='btn' ><AiFillCreditCard size='4rem'/>Card</Button>
+          <Button  className='btn' ><BsCash size='4rem'/> Cash</Button>
+          <Button  className='btn' ><HiCash size='4rem'/>Gcash</Button>
+          </div>
+
+
+
+        <div className='choices1'>
+         <Button className='btn' >Purchase</Button>
+         <Button onClick = {()=>{navigate('/Main');}} className='btn' >Cancel</Button>
+         </div>
+
+
+
+      </header>
+    </div>
+  );
+}
+export default Payment;
