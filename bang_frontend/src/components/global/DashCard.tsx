c59ddda839cf65f4929e032e649624c8280399ff
JOSHUA TABOSO <joshua.taboso@cit.edu>
2022-12-17 05:08:20 +0800

    Add DashCard component

---
+++ bang_frontend/src/components/global/DashCard.tsx
@@ -0,0 +1,37 @@
+import { Button, CardActions } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export interface CardDetails{
+    title:string;
+    image: string;
+    description: string;
+}
+
+export default function DashCard(props:CardDetails) {
+  return (
+    <Card sx={{ borderRadius:'10%', maxWidth: 345, textAlign: 'left', marginBottom:'1rem', marginRight: '1rem', width: 300, height: 400, boxShadow:'invert'}}>
+      <CardMedia
+        component="img"
+        height="240"
+        image={props.image}
+        alt="image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" sx={{textAlign: 'center'}}>
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{textAlign: 'center'}}>
+         {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" variant='contained'>
+          Proceed
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
