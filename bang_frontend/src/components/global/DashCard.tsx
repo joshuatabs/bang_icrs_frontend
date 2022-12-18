81c0831c9fa62762a0a1c93aa0f78afcbfe4382b
Corazeus <113650481+Corazeus@users.noreply.github.com>
2022-12-19 02:02:06 +0800

    Update Dashboard

--- bang_frontend/src/components/global/DashCard.tsx
+++ bang_frontend/src/components/global/DashCard.tsx
@@ -3,35 +3,37 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import '../css/DashCard.css'
 
-export interface CardDetails{
-    title:string;
-    image: string;
-    description: string;
+export interface CardDetails {
+  title: string;
+  image: string;
+  description: string;
+  link: string;
 }
 
-export default function DashCard(props:CardDetails) {
+export default function DashCard(props: CardDetails) {
   return (
-    <Card sx={{ borderRadius:'10%', maxWidth: 345, textAlign: 'left', marginBottom:'1rem', marginRight: '1rem', width: 300, height: 400, boxShadow:'invert'}}>
-      <CardMedia
-        component="img"
-        height="240"
-        image={props.image}
-        alt="image"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div" sx={{textAlign: 'center'}}>
-          {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{textAlign: 'center'}}>
-         {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary" variant='contained'>
-          Proceed
-        </Button>
-      </CardActions>
-    </Card>
+
+    <a href={props.link}>
+      <Card sx={{ borderRadius: '10%', maxWidth: 345, textAlign: 'left', marginBottom: '1rem', marginRight: '1rem', width: 300, height: 400, boxShadow: 'invert' }} className='card'>
+        <CardMedia
+          component="img"
+          height="240"
+          image={props.image}
+          alt="image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: 'center' }}>
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'center' }}>
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+        </CardActions>
+      </Card>
+    </a>
   );
 }
--- bang_frontend/src/components/pages/Dashboard.tsx
+++ bang_frontend/src/components/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
 import ResponsiveAppBar from '../global/navbar';
-import Card, { CardDetails } from '../global/Card';
+import Card, { CardDetails } from '../global/DashCard';
 
 export default function Dashboard() {
 
   const iCard:CardDetails[] = [
-    {title:'Rent a PC', image:'/static/images/Dashboard/pc_room.jpg', description:'Reserve your PC now and choose where you want to play.'},
-    {title:'Bang! Cafe', image:'/static/images/Dashboard/dark_cafe.jpg', description:'Feeling Hungry? Grab a bite and explore the Cafe menu.'},
-    {title:'Tournament Mode', image:'/static/images/Dashboard/tournament.jpg', description:'Rent the whole Venue for your tournament needs. Ready Set Play!'},
+    {title:'Rent a PC', image:'/static/images/Dashboard/pc_room.jpg', description:'Reserve your PC now and choose where you want to play.', link:'/roommenu'},
+    {title:'Bang! Cafe', image:'/static/images/Dashboard/dark_cafe.jpg', description:'Feeling Hungry? Grab a bite and explore the Cafe menu.', link:'/foodmainmenu'},
+    {title:'Tournament Mode', image:'/static/images/Dashboard/tournament.jpg', description:'Rent the whole Venue for your tournament needs. Ready Set Play!', link:'/rent'},
   ]
 
   return (
@@ -18,7 +18,7 @@ export default function Dashboard() {
         <br></br>
         <div className='CARDS'> 
           {iCard.map((item, i)=>
-            <Card key={i} title={item.title} image={item.image} description={item.description}/>
+            <Card key={i} title={item.title} image={item.image} description={item.description} link={item.link}/>
           )}
         </div>
       </header>
