348a4ff6811be34121ebc80588a6d452dc66a33b
Corazeus <113650481+Corazeus@users.noreply.github.com>
2022-12-19 21:01:12 +0800

    Update Room Choices

--- bang_frontend/src/components/pages/RoomChoices.tsx
+++ bang_frontend/src/components/pages/RoomChoices.tsx
@@ -6,11 +6,12 @@ function RoomChoices() {
 
     const NEW_URL = 'http://localhost:8080/room/postRoom';
 
-    const Room = async (code:string) => {
+    const Room = async (code:string, floor:string) => {
 
       axios
           .post(NEW_URL,{
             code,
+            floor,
           })
           .then(res => {
 
@@ -37,12 +38,12 @@ function RoomChoices() {
         <h1 className='select'> SELECT ROOM </h1>
 
         <div className='choices1'>
-          <button onClick={() => Room('Public Room')} className='btn'>Public Room</button> 
-          <button onClick={() => Room('Private Room')} className='btn'>Private Room</button> 
+          <button onClick={() => Room('Public Room', '1st Floor')} className='btn'>Public Room</button> 
+          <button onClick={() => Room('Private Room', '2nd Floor')} className='btn'>Private Room</button> 
         </div>
 
         <div className='choices2'>
-          <button onClick={() => Room('Tournament Room')} className='btn'>Tournament Room</button> 
+          <button onClick={() => Room('Tournament Room', '3rd Floor')} className='btn'>Tournament Room</button> 
         </div>
         <br></br>
 
