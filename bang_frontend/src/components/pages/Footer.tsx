49304666a8cc46cf6cb2f5fb5853aa6e87f5642d
K3ndi647 <118802814+K3ndi647@users.noreply.github.com>
2022-12-20 02:48:14 +0800

    Create Footer.tsx

---
+++ bang_frontend/src/components/pages/Footer.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    
+    render() {
+        return (
+            <div>
+                <footer className='footer'>
+                    <span className='text-muted' > All Rights Reserved 2022 @Bang</span>
+
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
